ab8f2124ce898c85502c0774079cf80e4dd7b91f
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-03-10 20:27:21 -0500

    Create CCC_91_J1.java

---
+++ CCC/CCC_91_J1.java
@@ -0,0 +1,24 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        byte apple3 = Byte.parseByte(br.readLine);
+        byte apple2 = Byte.parseByte(br.readLine);
+        byte apple1 = Byte.parseByte(br.readLine);
+        byte banana3 = Byte.parseByte(br.readLine);
+        byte banana2 = Byte.parseByte(br.readLine);
+        byte banana1 = Byte.parseByte(br.readLine);
+
+        if ((3*apple3+2*apple2+apple1)>(3*banana3+2*banana2+banana1)) {
+          System.out.println("A");
+        }
+        else if ((3*apple3+2*apple2+apple1)<(3*banana3+2*banana2+banana1)) {
+          System.out.println("B");
+        }
+        else {
+          System.out.println("T");
+        }
+    }
+}
