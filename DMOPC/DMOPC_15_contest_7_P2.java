f55716b8ae2419ac873708583dadbb5dae77b3f4
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-03-10 19:43:44 -0500

    Add files via upload

---
+++ DMOPC/DMOPC_15_contest_7_P2.java
@@ -0,0 +1,14 @@
+package test_dmoj;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class DMOPC_15_contest_7_P2 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String in = br.readLine();
+		System.out.println(in.trim().split("\\s+").length);
+	}
+
+}
