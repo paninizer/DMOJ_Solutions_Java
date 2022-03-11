0b159b64a6aaf650844f7e695ca2992aed838239
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-03-10 20:28:23 -0500

    Create CCC_10_J1.java

---
+++ CCC/CCC_10_J1.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		byte n = Byte.parseByte(st.nextToken());
+		int count =0;
+		for(int a=0;a<=5;a++) {
+			for(int b=0;b<=5;b++) {
+				if(a+b==n) {
+					if(a==b) {
+						count= count+1;
+					}
+					count++;
+				}
+			}
+		}
+		System.out.println(count/2);
+		
+    }
+}
