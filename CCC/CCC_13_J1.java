f13c9c0a8994fc738b5ea5c2cfb01ceb256a980c
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-03-10 20:54:04 -0500

    Create CCC_13_J1.java

---
+++ CCC/CCC_13_J1.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    byte Younger = Byte.parseByte(br.readLine);
+    byte Middle = Byte.parseByte(br.readLine);
+    System.out.println(2*Middle-Younger);
+
+    }
+}
