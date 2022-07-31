a4a4a552942d6be56b03a56e16e9ba908f6d1009
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-07-31 15:52:06 -0400

    Create Alpaca_Shapes.java

---
+++ An Animal Contest/Contest 1/Alpaca_Shapes.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+public class Alpaca_Shapes {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        String  lines = br.readLine();    
+        
+        String[] strs = lines.trim().split("\\s+");
+        double[] a = new double[2];
+        a[0] = Double.parseDouble(strs[0]);
+        a[1] = Double.parseDouble(strs[1]);
+        
+        double squarearea = Math.pow(a[0], 2);
+        double circlearea = Math.pow(a[1], 2)*3.14;
+        
+        if(squarearea>circlearea) {
+        	System.out.println("SQUARE");
+        }
+        else {
+        	System.out.println("CIRCLE");
+        }
+	}
+
+}
