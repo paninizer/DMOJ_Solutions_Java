31a614b00c877c794e06f3a7998da9eea70a4d47
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-07-31 12:12:03 -0400

    Fixed minor error
    Added throws IOException on line 4

--- CCC/2019/CCC_19_J1.java
+++ CCC/2019/CCC_19_J1.java
@@ -1,7 +1,7 @@
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         byte apple3 = Byte.parseByte(br.readLine);
