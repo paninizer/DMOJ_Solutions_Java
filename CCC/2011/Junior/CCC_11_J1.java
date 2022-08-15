7ba16ba5c9796207a7a84281ac12f0630665b927
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-15 11:55:11 -0400

    Fixing Errors

--- CCC/2011/Junior/CCC_11_J1.java
+++ CCC/2011/Junior/CCC_11_J1.java
@@ -5,8 +5,8 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        byte a = Byte.parseByte(br.readLine);
-        byte e = Byte.parseByte(br.readLine);
+        byte a = Byte.parseByte(br.readLine());
+        byte e = Byte.parseByte(br.readLine());
         if (a>=3&&e<=4) {
           System.out.println("TroyMartian");
         }
