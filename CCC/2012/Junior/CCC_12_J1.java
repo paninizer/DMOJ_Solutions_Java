bd302517b4f764dc70fbcdcac217d87ade1e773d
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-15 11:59:10 -0400

    Fixing Errors

--- CCC/2012/Junior/CCC_12_J1.java
+++ CCC/2012/Junior/CCC_12_J1.java
@@ -3,7 +3,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		short limit = Short.parseShort(br.readLine());
