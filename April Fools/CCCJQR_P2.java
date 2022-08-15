48317cf1f63d1f15f80c4be1d552e6e46397f712
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-15 12:20:21 -0400

    Create CCCJQR_P2.java

---
+++ April Fools/CCCJQR_P2.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		@SuppressWarnings("unused")
+		String[] jc = br.readLine().split(" ");
+		
+		int num = Integer.parseInt(br.readLine());
+		System.out.println(num);
+    }
+}
