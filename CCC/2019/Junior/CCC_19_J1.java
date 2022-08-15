67e1b1a3ff6c49463cd0959e73f9a46b7759866b
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-15 12:08:19 -0400

    Fixing Errors

--- CCC/2019/Junior/CCC_19_J1.java
+++ CCC/2019/Junior/CCC_19_J1.java
@@ -4,12 +4,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        byte apple3 = Byte.parseByte(br.readLine);
-        byte apple2 = Byte.parseByte(br.readLine);
-        byte apple1 = Byte.parseByte(br.readLine);
-        byte banana3 = Byte.parseByte(br.readLine);
-        byte banana2 = Byte.parseByte(br.readLine);
-        byte banana1 = Byte.parseByte(br.readLine);
+        byte apple3 = Byte.parseByte(br.readLine());
+        byte apple2 = Byte.parseByte(br.readLine());
+        byte apple1 = Byte.parseByte(br.readLine());
+        byte banana3 = Byte.parseByte(br.readLine());
+        byte banana2 = Byte.parseByte(br.readLine());
+        byte banana1 = Byte.parseByte(br.readLine());
 
         if ((3*apple3+2*apple2+apple1)>(3*banana3+2*banana2+banana1)) {
           System.out.println("A");
