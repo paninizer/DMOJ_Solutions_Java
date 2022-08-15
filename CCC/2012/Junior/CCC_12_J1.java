6f5e0c32da0c7f984267411f7634c30689d2c0bd
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-15 11:58:42 -0400

    Fixing Errors

--- CCC/2012/Junior/CCC_12_J1.java
+++ CCC/2012/Junior/CCC_12_J1.java
@@ -5,24 +5,24 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) {
 
-      BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	    short limit = Short.parseShort(br.readLine);
-	    short speed = Short.parseShort(br.readLine);
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		short limit = Short.parseShort(br.readLine());
+		short speed = Short.parseShort(br.readLine());
 
-	    short overspeed = speed-limit;
+		short overspeed = (short) (speed-limit);
 
-	    if (overspeed<=0) {
-	    	System.out.println("Congratulations, you are within the speed limit!");
-	    }
-	    else if (1 <= overspeed && overspeed <=20) {
-	    	System.out.println("You are speeding and your fine is $100.");
-	    }
-	    else if (21 <= overspeed && overspeed <=30) {
-	    	System.out.println("You are speeding and your fine is $270.");
-	    }
-	    else {
-	    	System.out.println("You are speeding and your fine is $500.");
-	    }
+		if (overspeed<=0) {
+			System.out.println("Congratulations, you are within the speed limit!");
+		}
+		else if (1 <= overspeed && overspeed <=20) {
+			System.out.println("You are speeding and your fine is $100.");
+		}
+		else if (21 <= overspeed && overspeed <=30) {
+			System.out.println("You are speeding and your fine is $270.");
+		}
+		else {
+			System.out.println("You are speeding and your fine is $500.");
+		}
 
     }
 }
