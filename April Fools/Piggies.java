c29c1cad7e56328c3d5be9297f7f9d307626a5bc
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-15 11:36:58 -0400

    Create Piggies.java

---
+++ April Fools/Piggies.java
@@ -0,0 +1,12 @@
+public class Piggies {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("    ^/\\^    \r\n"+
+                "   (*??*)   \r\n"+
+                "^==(____)==^\r\n"+
+                "  ._|  |_.   \r\n");
+	}
+}
+
+//Apparently the longer you spend on the editor, the better your score will be, regardless of how ugly the pig is
