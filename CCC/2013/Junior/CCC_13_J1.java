d458cb16db361be799b9a466834715ee59b049e7
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-15 12:01:19 -0400

    Fixing Errors

--- CCC/2013/Junior/CCC_13_J1.java
+++ CCC/2013/Junior/CCC_13_J1.java
@@ -5,8 +5,8 @@ import java.io.BufferedReader;
 public class Main {
     public static void main(String[] args) throws IOException {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    byte Younger = Byte.parseByte(br.readLine);
-    byte Middle = Byte.parseByte(br.readLine);
+    byte Younger = Byte.parseByte(br.readLine());
+    byte Middle = Byte.parseByte(br.readLine());
     System.out.println(2*Middle-Younger);
 
     }
