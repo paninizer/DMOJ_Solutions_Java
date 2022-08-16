d1652ee5aee46f0ead69ce1326d1d52b52896a7b
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-16 17:28:23 -0400

    Create AFD1P4.java

---
+++ April Fools/April Fools Day Contest/Contest 1/AFD1P4.java
@@ -0,0 +1,11 @@
+//This isn't a solvable problem, just try to get the highest score between 0 and 99
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(98.9999999999999); //the decimals behind must be an unchosen number (not that one in my solution), like 98.12858091823
+    }
+}
