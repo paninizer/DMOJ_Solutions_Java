42e3f655522ba7e0421e18a2d71940f086cc9f5b
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-16 17:32:27 -0400

    Update AFD1P4.java

--- April Fools/April Fools Day Contest/Contest 1/AFD1P4.java
+++ April Fools/April Fools Day Contest/Contest 1/AFD1P4.java
@@ -7,5 +7,6 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println(98.9999999999999); //the decimals behind must be an unchosen number (not that one in my solution), like 98.12858091823
+        //you can output 0 (but why)
     }
 }
