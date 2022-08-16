95a2a6afb6434fbef93320ba81363069573ebf22
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-16 17:34:09 -0400

    Update AFD1P4.java

--- April Fools/April Fools Day Contest/Contest 1/AFD1P4.java
+++ April Fools/April Fools Day Contest/Contest 1/AFD1P4.java
@@ -8,5 +8,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(98.9999999999999); //the decimals behind must be an unchosen number (not that one in my solution), like 98.12858091823
         //you can output 0 (but why)
+        //literal numbers are not allowed, and will return presentation error (e.g. Ninety-Nine)
     }
 }
