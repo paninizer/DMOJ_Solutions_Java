21b3f0035e050b0282bc506464b8c1540071e54b
panzer-chan <101371138+panzer-chan@users.noreply.github.com>
2022-08-26 16:05:07 -0400

    Formatted code
    Indented code for improved readability

--- An Animal Contest/Contest 1/Alpaca_Shapes.java
+++ An Animal Contest/Contest 1/Alpaca_Shapes.java
@@ -5,24 +5,24 @@ public class Alpaca_Shapes {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        String  lines = br.readLine();    
+        	String  lines = br.readLine();    
         
-        String[] strs = lines.trim().split("\\s+");
-        double[] a = new double[2];
-        a[0] = Double.parseDouble(strs[0]);
-        a[1] = Double.parseDouble(strs[1]);
+        	String[] strs = lines.trim().split("\\s+");
+        	double[] a = new double[2];
+        	a[0] = Double.parseDouble(strs[0]);
+        	a[1] = Double.parseDouble(strs[1]);
         
-        double squarearea = Math.pow(a[0], 2);
-        double circlearea = Math.pow(a[1], 2)*3.14;
+        	double squarearea = Math.pow(a[0], 2);
+        	double circlearea = Math.pow(a[1], 2)*3.14;
         
-        if(squarearea>circlearea) {
-        	System.out.println("SQUARE");
-        }
-        else {
-        	System.out.println("CIRCLE");
-        }
+        	if(squarearea>circlearea) {
+        		System.out.println("SQUARE");
+        	}
+        	else {
+        		System.out.println("CIRCLE");
+        	}
 	}
 
 }
